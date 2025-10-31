026457fb5e7b52d1e4846f42c18e980400e09fb2
RajMaheshwari <rajmaheshwar08@gmail.com>
2025-10-31 22:18:27 +0000

    Cleaned up prompt

--- BiasGuard/config.py
+++ BiasGuard/config.py
@@ -1,3 +1,7 @@
 FINETUNE_DATA_PATH = "finetune_data.jsonl"
+
 PROMPT_TEMPLATE_PATH = "prompt_templates/original.json"
-TEACHER_MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+BIAS_TYPES_PATH = "prompt_templates/bias_types.json"
+STANDARDS_PATH = "prompt_templates/standards.json"
+
+TEACHER_MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B"
--- BiasGuard/generate_finetune_data.py
+++ BiasGuard/generate_finetune_data.py
@@ -1,19 +1,37 @@
 import json
+import torch
 
-from config import FINETUNE_DATA_PATH, PROMPT_TEMPLATE_PATH
+from config import *
 from transformers import pipeline
 from tqdm.auto import tqdm
-
 
 def create_prompt():
     print("Loading prompt template from:", PROMPT_TEMPLATE_PATH)
 
+    with open("prompt_templates/bias_types.json", 'r') as f:
+        bias_dict = json.load(f)
+        bias_types = ""
+        for bias_name, bias in bias_dict.items():
+            bias_types += "\n" + bias_name + ":\n"
+            for k, v in bias.items():
+                bias_types += f"{k}: {v}\n"
+    
+    with open("prompt_templates/standards.json", 'r') as f:
+        standards_dict = json.load(f)
+        standards = ""
+        for _, standard in standards_dict.items():
+            standards += "\n"
+            for k, v in standard.items():
+                standards += f"{k}: {v}\n"
+
     with open(PROMPT_TEMPLATE_PATH, 'r') as f:
         prompt_template = json.load(f)
+        prompt = ""
+        for key, value in prompt_template.items():
+            value = value.replace("{standards}", standards)
+            value = value.replace("{bias_types}", bias_types)
+            prompt += f"{key}\n{value}\n\n"
 
-    prompt = ""
-    for key, value in prompt_template.items():
-        prompt += f"{key}\n{value}\n\n"
     return prompt
 
 
@@ -21,7 +39,8 @@ def generate_finetune_data(prompt):
     pipe = pipeline("text-generation",
                     model=TEACHER_MODEL_NAME)
     messages = [
-        {"role": "user", "content": "Who are you?"},
+        {"role": "system", "content": prompt},
+        {"role": "user", "content": "jews are the only group of people who are told that they are not allowed to have a homeland in the same place they have lived for thousands of years."},
     ]
     print(pipe(messages))
 
@@ -31,6 +50,6 @@ def save_finetune_data(data, path=FINETUNE_DATA_PATH):
 
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     prompt = create_prompt()
-    print(prompt)
-    # generate_finetune_data(prompt)
+    generate_finetune_data(prompt)
