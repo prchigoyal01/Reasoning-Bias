e473a01ba71b2b3a1f59769e752a931e783c4b48
RajMaheshwari <rajmaheshwar08@gmail.com>
2025-11-09 00:28:47 -0500

    batching

--- BiasGuard/config.py
+++ BiasGuard/config.py
@@ -5,3 +5,5 @@ BIAS_TYPES_PATH = "prompt_templates/bias_types.json"
 STANDARDS_PATH = "prompt_templates/standards.json"
 
 TEACHER_MODEL_NAME = "deepseek-ai/DeepSeek-R1-Distill-Qwen-14B"
+BATCH_SIZE = 20
+TOTAL_SAMPLES = 5000
--- BiasGuard/generate_finetune_data.py
+++ BiasGuard/generate_finetune_data.py
@@ -4,10 +4,9 @@ import torch
 from config import *
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from tqdm.auto import tqdm
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 import pandas as pd
 import regex as re
-
 
 def get_system_instruction():
     print("Loading prompt template from:", PROMPT_TEMPLATE_PATH)
@@ -39,15 +38,25 @@ def get_system_instruction():
     return prompt
 
 
+def format_prompt(example, sys_inst):
+    messages = [
+        {"role": "SYSTEM", "content": sys_inst},
+        {"role": "USER", "content": example["prompt"]},
+    ]
+    text = "\n".join([f"[{m['role']}]: {m['content']}" for m in messages])
+    text += "\n[ASSISTANT]: "
+    return {"prompt_text": text}
+
+
 def generate_finetune_data():
+    sys_inst = get_system_instruction()  # Evaluate sys_inst only once
     ds = load_dataset("toxigen/toxigen-data", "train")
-    ds = ds['train'].shuffle(seed=37).select(range(500))
+    ds = ds['train'].shuffle(seed=42).select(range(TOTAL_SAMPLES))
+    dataset = Dataset.from_list(ds)
+    dataset = dataset.map(lambda example: format_prompt(example, sys_inst))
     
     biased = "Yes, the sentence is biased."
     unbiased = "No, the sentence is unbiased."
-
-    sys_inst = get_system_instruction()
-
     bnb_config_4bit = BitsAndBytesConfig(
         load_in_4bit=True,
         bnb_4bit_use_double_quant=True,
@@ -65,48 +74,59 @@ def generate_finetune_data():
     model = AutoModelForCausalLM.from_pretrained(
         TEACHER_MODEL_NAME,
         device_map="auto", # Automatically distributes the model across available GPUs
-        quantization_config = bnb_config_4bit,
+        quantization_config = bnb_config_8bit,
         # offload_folder="./offload",       # folder for CPU offload weights
     )
     tokenizer = AutoTokenizer.from_pretrained(TEACHER_MODEL_NAME)
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_new_tokens=1024, # paper specified 2048, but too slow.
-        temperature=1.2, # specified in the paper
-    )
 
-    finetune_rows = []
-    for data in tqdm(ds):
-        # torch.cuda.empty_cache()
-        query = data['prompt']
-        label = data['prompt_label']
+    batch_size = BATCH_SIZE
+    for i in tqdm(range(0, len(dataset), batch_size)):
+        results = []
+        batch = dataset[i:i+batch_size]
+        inputs = tokenizer(
+            batch["prompt_text"],
+            return_tensors="pt",
+            padding=True,
+            truncation=True,
+            add_special_tokens=False,
+        ).to(model.device)
 
-        messages = [
-            {"role": "system", "content": sys_inst},
-            {"role": "user", "content": query},
-        ]
+        input_lens = [len(prompt) for prompt in inputs['input_ids']]
+        with torch.no_grad():
+            outputs = model.generate(
+                **inputs,
+                max_new_tokens=2048, # specified in the paper
+                temperature=0.7,
+                do_sample=True,
+            )
+        decoded = tokenizer.batch_decode(
+            [out[input_lens[i]:] for i, out in enumerate(outputs)],
+            skip_special_tokens=True
+        )
 
-        resp = pipe(messages)[-1]
-        output = resp["generated_text"][-1]["content"]
-        conclusion = re.search(r"## Conclusion ##:\s*(.*)", output)
-        if not conclusion:
-            continue        
-        conclusion = conclusion.group(1).strip()
+        print(tokenizer.batch_decode(
+            [out for i, out in enumerate(outputs)],
+            skip_special_tokens=True
+        ))
 
-        if (label == 1 and biased in conclusion and not (unbiased in conclusion)) or (label == 0 and unbiased in conclusion and not (biased in conclusion)):
-            row = {
-                'prompt': query,
-                'label': label,
-                'response': output,
-                'conclusion': conclusion
-            }
-            finetune_rows.append(row)
+        for prompt, prompt_label, output in zip(batch['prompt'], batch['prompt_label'], decoded):
+            conclusion = re.search(r"## Conclusion ##:\s*(.*)", output)
+            if conclusion:
+                conclusion = conclusion.group(1).strip()
+                print(conclusion)
+                if (prompt_label == 1 and biased in conclusion and not (unbiased in conclusion)) \
+                or (prompt_label == 0 and unbiased in conclusion and not (biased in conclusion)):
+                    finetune_row = {
+                        "prompt": prompt,
+                        "label": prompt_label,
+                        "response": output,
+                        "conclusion": conclusion
+                    }
+                    results.append(finetune_row)
 
-    with open(FINETUNE_DATA_PATH, 'w') as f:
-        for row in finetune_rows:
-            f.write(json.dumps(row) + '\n')
+        with open(FINETUNE_DATA_PATH, "a") as f:
+            for row in results:
+                f.write(json.dumps(row) + "\n")
 
 
 if __name__ == "__main__":
